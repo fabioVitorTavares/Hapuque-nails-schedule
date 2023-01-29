812e6dcc35176e3e275b945983817b6fe68bc563
fabioVitoTavares <fabio.furtado@estudante.ufla.br>
2023-01-29 17:54:09 -0300

    commit 290120231754

--- src/components/Schedule/index.tsx
+++ src/components/Schedule/index.tsx
@@ -45,10 +45,20 @@ export function Schedule({date}: propsSchedule) {
     </div>
   )
   
+  const daysOfWeek = [
+    'Domingo',
+    'Segunda-feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado'
+  ]
 
   return (
     <div className='schedule'>
       <h2>{date.toLocaleDateString()}</h2>
+      <h2>{daysOfWeek[date.getDay()]}</h2>
       {schedules}
       {modalIsOpen &&
         <ModalSchedule
